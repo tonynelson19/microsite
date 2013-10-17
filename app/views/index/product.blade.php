cdc3f10e7d865d0bf2f26c87bf7e631d0d017c19
Tony Nelson <tonynelson19@gmail.com>
2013-10-16 22:48:35 -0500

    Updated video modal

--- app/views/index/product.blade.php
+++ app/views/index/product.blade.php
@@ -75,6 +75,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-body">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                         <iframe width="560" height="415" src="{{ Video::getYouTubeEmbedUrl($product->videos[0]->videoUrl) }}?wmode=transparent&html5=1" frameborder="0" allowfullscreen></iframe>
                     </div>
                 </div>
